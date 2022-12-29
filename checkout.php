37f52133916c4d7bb022a6e23564205645335e2f
Anwar Shaikh <anwar.squarebiz@gmail.com>
2022-12-30 04:37:43 +0530

    Payment integration completed.

---
+++ checkout.php
@@ -0,0 +1,216 @@
+<!-- Include navbar -->
+<?php include 'includes/header.php'; ?>
+
+<body>
+    <!-- Include navbar -->
+    <?php include 'includes/navbar.php'; ?>
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                    Advertising Creative Services:
+                </h1>
+                <div class="mt-3">
+                    The advertising division, being the oldest division is changing with the new advancements in technology and creative services in the industry. The Division has always been associated with top Directors, Production houses and Ad agencies in the industry. Creativewise entertainment is always preferred by clients in the industry because of the creative and in-house expert artists. Apart from post-production (Offline, Online and DI) services, the division offers VFX/CGI, Digital Production, Animatics, Stop Motion, Adaptation & Versioning, 2D & 3D single frame services. The division delivered post production services for 4 ABBY Award winners in 2018 (1 Gold, 1 Silver & 2 Bronze)
+                </div>
+    
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 394000* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php?">
+                    <input hidden name="amount" value="394000"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                    Production Services:
+                </h1>
+                <div class="mt-3">
+                Creativewise Entertainment is India’s largest provider of camera equipment rental services and Media Services business with Creativewise Entertainment Private Limited.
+                </div>
+
+                <div class="mt-3">
+                    We are India’s largest camera equipment rental services provider. With a dedicated 10,000 square foot state-of-the-art facility, we offer innovative digital cinema solutions, latest high-end cameras and equipment including expert technical support to film makers. Prime Focus was the first to introduce cameras like Alexa, Sony F65, Red Epic and Red Scarlet to the Indian market.
+                </div>
+    
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 682000* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php?">
+                    <input hidden name="amount" value="682000"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                    Post Production Services:
+                </h1>
+                <div class="mt-3">
+                Creativewise Entertainment is a global post production major with facilities in Mumbai, New York and London catering to local content markets. It has a highly talented team of professionals offering Digital Intermediate/color grading, sound and picture post.
+                </div>
+
+                <h1 class="checkout-package-heading mt-3">
+                    Digital Intermediate/Color Grading
+                </h1>
+                <div>
+                    Creativewise Entertainment has color graded more than 225+ feature films till date. We introduced India to DI technology. Our expertise has been widely acclaimed with numerous industry awards.
+                </div>
+
+                <h1 class="checkout-package-heading mt-3">
+                    Picture Post
+                </h1>
+                <div>
+                    For advertisement spots and broadcast content we are the leading post facility across markets. Our work has won multiple Emmy® awards, Abby® awards and numerous other industry recognition over the years.
+                </div>
+    
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 625000* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php?">
+                    <input hidden name="amount" value="625000"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                    Commercial/Feature Film services:
+                </h1>
+                <div class="mt-3">
+                    We give productions a head start by providing in-depth research and transparent budgeting. We then assist in permits, location scouting, multi-city casting, travel & visas.
+                </div>
+                <div class="mt-3">
+                    We source the right multilingual crew & technicians for the job. We have access to state of the art film equipment, production design & studio spaces.
+                </div>
+
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 558000* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php?">
+                    <input hidden name="amount" value="558000"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                Pre production services:
+                </h1>
+                <div class="mt-3">
+                    including concept development, production, and post production. For those who need an all-in-one script-to-screen solution, Film Creations can write, produce and edit a finished product that fits your video objectives.
+                </div>
+                <div class="mt-3">
+                    Whether you are shooting on location or at our studio, our state-of-the-art facilities for video production in Tucson have everything you need from pre-production to final edits. We offer high quality, attention-getting results that are designed to meet and achieve the specific goals of each client.
+                </div>
+                <ul>
+                    <li>
+                    3-D, 2-D & Motion Graphics Editing
+                    </li>
+                    <li>
+                    Green Screen Capabilities
+                    </li>
+                    <li>
+                    Concept, Scripting & Creative Assistance
+                    </li>
+                    <li>
+                    Full HD Camera Packages
+                    </li>
+                    <li>
+                    Full Audio Recording Capabilities & Royalty-Free Music Library
+                    </li>
+                </ul>
+
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 917960* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php?">
+                    <input hidden name="amount" value="917960"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 mt-3">
+                <h1 class="checkout-package-heading">
+                Web series production Materials services:
+                </h1>
+                <div class="mt-3">
+                Netflix may request miscellaneous production material to assist in the localization of licensed content. The following “Asset Types” may be requested:    
+                </div>
+
+                <ul>
+                    <li>
+                    Printmaster Audio*
+                    </li>
+                    <li>
+                    Music & Effects (M&E)*
+                    </li>
+                    <li>
+                    Dialogue Music & Effects Stems (DME)
+                    </li>
+                    <li>
+                    Language Template*
+                    </li>
+                    <li>
+                    Dialogue List
+                    </li>
+                    <li>
+                    Dub Script*
+                    </li>
+                    <li>
+                    KNPs
+                    </li>
+                    <li>
+                    Show Guides
+                    </li>
+                    <li>
+                    Locked Proxy*
+                    </li>
+                </ul>
+
+                <div class="mt-3">
+                    * These Asset Types can be requested via Source Requests within Backlot. For all others please confirm the method of delivery with your Netflix representative. If you are issued a Source Request for one of the above Asset Types that you do not have access to, please contact your Netflix representative.
+                </div>
+
+                <h1 class="checkout-package-heading mt-3">
+                    Price : INR 1032000* Starting from
+                </h1>
+                <form class="text-center mt-3" action="/CCAvenue/CUSTOM_CHECKOUT_FORM_KIT/dataFrom.php">
+                    <input hidden name="amount" value="1032000"></input>
+                    <button class="btn btn-dark">
+                        Pay Now
+                    </button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Include Footer -->
+    <?php
+        include "includes/footer.php";
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+
+        </body>
+
+</html>
